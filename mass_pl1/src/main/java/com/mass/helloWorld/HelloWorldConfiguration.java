c173291a6cbfd1ccaf9eff4fd36a6bbb1b7d7675
Anandhu <you@example.com>
2017-04-09 02:54:45 +0530

    First sample in Dropwizard completed.

---
+++ mass_pl1/src/main/java/com/mass/helloWorld/HelloWorldConfiguration.java
@@ -0,0 +1,41 @@
+package com.mass.helloWorld;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+public class HelloWorldConfiguration extends Configuration {
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
+
+//    @Valid
+//    @NotNull
+//    private DataS1ourceFactory database = new DataSourceFactory();
+}
